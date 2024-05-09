5fbf65cd8adc09156fab445cfe8ec62dcea1a460
Vittório de Andrade Barella <vittorio.br@hotmail.com>
2024-05-09 16:15:15 -0300

    deleted and organized some files

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import React from "react";
+import List from './List'
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Board Component</p>
+                <List />
+                <List />
+            </div>
+        )
+    }
+}
+
+export default Board;
