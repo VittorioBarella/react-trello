07e5f9d2e757a17e283c86f3d18f847ebc310fb2
Vittório de Andrade Barella <vittorio.br@hotmail.com>
2024-05-14 16:01:30 -0300

    added the css style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BoardPreview from '../BoardPreview'
+import PropTypes from 'prop-types'
 
 class Home extends React.Component {
     newBoard = () => {
@@ -26,6 +27,11 @@ class Home extends React.Component {
             </div>
         )
     }
+}
+
+Home.propTypes = {
+    boards: PropTypes.array.isRequired,
+    createNewBoard: PropTypes.func.isRequired,
 }
 
 export default Home
