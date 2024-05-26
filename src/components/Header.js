79e11ed942444b6c8fda64648cf4a5035e392209
Vittório de Andrade Barella <vittorio.br@hotmail.com>
2024-05-26 19:14:04 -0300

    created Header component and functions

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AuthConsumer } from './AuthContext'
+
+const Header = () => (
+	<header>
+		<AuthConsumer>
+			{({ user, logOut }) => (
+				<React.Fragment>
+					<a href={user.id ? `/${user.id}/boards` : `/`}>
+						<span role="img" aria-label="house emoji">&#127968;</span>
+					</a>
+					<h1>React Trello </h1>
+
+					<div className="user-area">
+						{user.id ? (
+							<React.Fragment>
+								<small>User: {user.email}</small>
+								<button onClick={(e) => logOut(e)}>Log out</button>
+							</React.Fragment>
+						) : (
+								<small>Please sign in</small>
+							)}
+					</div>
+				</React.Fragment>
+			)}
+		</AuthConsumer>
+	</header>
+)
+
+export default Header
